fc4cc0435282baead6d72b70ee5e10dcf38b3bff
gab007star <gabriele.portuesi@capellinisauro.edu.it>
2025-05-12 07:20:56 +0000

    Aggiunta stampa da VECTOR

--- HelloGabriele.java
+++ HelloGabriele.java
@@ -2,6 +2,20 @@ import java.io.*;
 public class HelloGabriele {
 public static void main(String[] args){
     System.out.println("Hello gabriele");
+
+Vector <character> lettere=new Vector<>();
+lettere.add('g');
+lettere.add('a');
+lettere.add('b');
+lettere.add('r');
+lettere.add('i');
+lettere.add('e');
+lettere.add('l');
+lettere.add('e');
+
+for(Character lettera: lettere){
+    System.out.println(lettera);
+}
     
 }
     
